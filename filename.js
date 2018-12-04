30cf9a9794bd4aacfbf4bbc3dae2d0afb3d463a8
HeleneLioussou <helene.lioussou@gmail.com>
2018-12-04 18:45:28 +0100

    taille de la tour eiffel

--- filename.js
+++
@@ -1,6 +0,0 @@
-$(window).on("scroll", function() {
-    var currentScroll = $(this).scrollTop();
-    if ($(currentScroll <= 50)) {
-        $("#eiffel").css("transform", "translate3d(0," + -currentScroll + "px, 0");
-    }
-});
